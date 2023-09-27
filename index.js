7ec6d923fc296b48a7ee1dacd2bd914c6124ba34
zbytes2227 <zbytes1@outlook.com>
2023-09-27 12:40:02 +0530

    first changes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Import the cors package
 const app = express();
+const PORT = process.env.PORT || 3030;
+
 
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
